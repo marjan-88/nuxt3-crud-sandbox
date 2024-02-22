baface5883285ccc3881adc3d5d3c248d25e7bf6
Mariusz <mariusz.maleszka@gmail.com>
2024-02-22 21:53:12 +0100

    [Add] - tooltip merges

--- pages/map.vue
+++ pages/map.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="my-map">
-    <LMap ref="map" :zoom="zoom" :center="[52.147, 21.028]">
+    <LMap ref="map" :zoom="zoom" :center="[52.147, 21.028]" @click="showTooltip">
       <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
         layer-type="base" name="OpenStreetMap" />
          <!-- Add markers for each point -->
-      <LMarker v-for="point in mapPoints" :key="point._id?.toString()" :lat-lng="[point.lat, point.lng]">
-        <LPopup>{{ point.name }} <br> {{ point.city }}</LPopup>
+         <LMarker v-for="(points, key) in groupedMapPoints" :key="key" :lat-lng="points[0]">
+          <LPopup>
+            <div v-for="point in points" :key="point._id?.toString()">
+              <NuxtLink :to="`/manage-points/${point._id}`">
+                <p>{{ point.name }} - {{ point.city }}</p>
+              </NuxtLink>
+            </div>
+          </LPopup>
       </LMarker>
     </LMap>
   </div>
@@ -15,14 +21,29 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useMapPointsStore } from '~/stores/MapPointsStore';
+import type { MapPoint } from '~/types/MapPoint';
 const zoom = ref(6);
-
 const mapPointsStore = useMapPointsStore();
 const {
   mapPoints,
   isLoading
 } = storeToRefs(mapPointsStore);
 
+
+const groupedMapPoints = computed(() => {
+  const grouped: { [key: string]: MapPoint[] } = {};
+  mapPoints.value.forEach(point => {
+    const key = `${point.lat},${point.lng}`;
+    if (!grouped[key]) {
+      grouped[key] = [];
+    }
+    grouped[key].push(point);
+  });
+  return grouped;
+});
+const showTooltip = (e:any)=> {
+  console.log(e.latlng.lat + ", " + e.latlng.lng);
+}
 
 </script>
   
